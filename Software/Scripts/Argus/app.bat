33fce753fce4ebf8d6f34f6d0612484be05d9364
Adam <help@panoptes.live>
2022-06-07 15:47:33 +1200

    More software
    Added Aria2c
    
    Create app.bat

---
+++ Software/Scripts/Argus/app.bat
@@ -0,0 +1,27 @@
+::
+:: Argus Operator | help@panoptes.live
+::
+:: July 15, 1990 | July 15, 1990
+::
+:: Example BATCH file
+:: Left Date - Document Created
+:: Right Date - Document Updated
+::
+
+
+:: Name terminal window
+TITLE Argus Panoptes Initialization
+
+
+:1
+:: Start
+
+:: Synergy
+
+:: Pauses CMD and waits for button press to continue
+PAUSE
+
+
+:0
+:: Tells CMD to Exit
+EXIT
